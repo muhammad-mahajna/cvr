97373806c279fea6914cd1b4d8de3c364e0004e9
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-10-31 16:38:12 -0600

    updated DICOM to NIFTII conversion script

--- src/preprocessing/convert_dicom_to_nifti.sh
+++ src/preprocessing/convert_dicom_to_nifti.sh
@@ -6,35 +6,38 @@
 # chmod +x ./convert_dicom_to_nifti.sh
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <base_dir> <nifti_dir> <search_term>"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <dicom_dir> <base_out_dir> <NIFTI_BASE_DIR> <search_term> <file_suffix>"
     exit 1
 fi
 
 # Define input parameters
-BASE_DIR="$1"      # Base directory where all subject folders are located
-NIFTI_DIR="$2"     # Directory where the converted NIfTI files will be saved
-SEARCH_TERM="$3"   # Subfolder name to search for within each subject folder
+BASE_DICOM_DIR="$1"      # Base directory where all subject folders are located
+NIFTI_BASE_DIR="$2"     # Directory where the converted NIfTI files will be saved
+OUT_SUB_DIR="$3"     # Directory where the converted NIfTI files will be saved
+SEARCH_TERM="$4"   # Subfolder name to search for within each subject folder
+FILE_SUFFIX="$5"   # Subfolder name to search for within each subject folder
 
 # Log the input parameters for reference
-echo "Base directory (DICOM): $BASE_DIR"
-echo "NIfTI output directory: $NIFTI_DIR"
+echo "Base directory (DICOM): $BASE_DICOM_DIR"
+echo "NIfTI output directory: $NIFTI_BASE_DIR"
 echo "Searching for subfolders containing: $SEARCH_TERM"
+echo "Using $FILE_SUFFIX as a file name suffix"
 
 # Create output directory if it doesn't exist
-mkdir -p "$NIFTI_DIR"
+mkdir -p "$NIFTI_BASE_DIR"
 
 # Start timestamp
 START_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 echo "Conversion started at: $START_TIME"
 
 # Convert DICOM files to NIfTI format for each subject's specified subfolder
-for subject_path in "$BASE_DIR"/*; do
+for subject_path in "$BASE_DICOM_DIR"/*; do
     if [ -d "$subject_path" ]; then
         subject_name=$(basename "$subject_path")
         
         # Define the expected NIfTI output file path
-        output_nifti_file="$NIFTI_DIR/${subject_name}.nii.gz"
+        output_nifti_file="$NIFTI_BASE_DIR/$subject_name/$OUT_SUB_DIR/${subject_name}_${FILE_SUFFIX}.nii.gz"
 
         # Check if the NIfTI file already exists
         if [ -f "$output_nifti_file" ]; then
@@ -42,18 +45,23 @@ for subject_path in "$BASE_DIR"/*; do
             continue
         fi
         
+        mkdir -p "$NIFTI_BASE_DIR/$subject_name/$OUT_SUB_DIR/"
+
         # Find the specified subfolder within the subject's directory
-        target_folder=$(find "$subject_path" -type d -name "*$SEARCH_TERM*" | head -n 1)
+        target_folder=$(find "$subject_path" -type d -name "*$SEARCH_TERM" | head -n 1)
 
         if [ -z "$target_folder" ]; then
-            echo "Warning: No '$SEARCH_TERM' folder found for subject $subject_name. Skipping."
-            continue
+            target_folder=$(find "$subject_path" -type d -name "*$SEARCH_TERM*" | head -n 1)
+            if [ -z "$target_folder" ]; then
+                echo "Warning: No '$SEARCH_TERM' folder found for subject $subject_name. Skipping."
+                continue
+            fi
         fi
 
         echo "Converting DICOM files for subject $subject_name from $target_folder..."
 
         # Run conversion using dcm2niix and name output file after subject ID
-        dcm2niix -z y -f "${subject_name}" -o "$NIFTI_DIR" "$target_folder"
+        dcm2niix -z y -f "${subject_name}_${FILE_SUFFIX}" -o "$NIFTI_BASE_DIR/$subject_name/$OUT_SUB_DIR" "$target_folder"
         
         # Check if the conversion was successful
         if [ $? -eq 0 ]; then
--- src/preprocessing/prepare_raw_data.sh
+++ src/preprocessing/prepare_raw_data.sh
@@ -1,12 +1,22 @@
 
 # Pipeline for preparing raw data 
 # Do that locally since the files are not available on the cluster
+# You can run each one of the following scripts separatly to speed things up
+
+RAW_DATA_BASE_DIR="../../../../../network_drive"
+BASE_DIR="../../../../data/cvr/"
+RSBOLD_DIR_ET="rsBOLD_ET"
+RSBOLD_DIR_ET_FLIP="rsBOLD_ET_Flip"
+T1_DIR="T1"
 
 echo "Convert rsBOLD images from DICOM to NIFTII format"
-./convert_dicom_to_nifti.sh ../../../../../network_drive ../../../../data/cvr/rsBOLD_ET rsBOLD-End-tidal
+./convert_dicom_to_nifti.sh $RAW_DATA_BASE_DIR $BASE_DIR $RSBOLD_DIR_ET rsBOLD-End-tidal $RSBOLD_DIR_ET
+
+echo "Convert rsBOLD images from DICOM to NIFTII format"
+./convert_dicom_to_nifti.sh $RAW_DATA_BASE_DIR $BASE_DIR $RSBOLD_DIR_ET_FLIP rsBOLD-End-tidal-_Flip_PE $RSBOLD_DIR_ET_FLIP
 
 echo "Convert T1 images from DICOM to NIFTII format"
-./convert_dicom_to_nifti.sh ../../../../../network_drive ../../../../data/cvr/T1 SAG_FSPGR_BRAVO
+./convert_dicom_to_nifti.sh $RAW_DATA_BASE_DIR $BASE_DIR $T1_DIR SAG_FSPGR_BRAVO $T1_DIR
 
 echo "2. Prepare reference CVR maps"
 

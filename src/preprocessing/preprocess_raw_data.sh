c194840369e61caacc71616f98420f9fba69e7d6
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-10-31 20:52:57 -0600

    updated preprocessing script for fmr and anatomical data files

--- src/preprocessing/preprocess_raw_data.sh
+++ src/preprocessing/preprocess_raw_data.sh
@@ -1,8 +1,6 @@
-RAW_DATA_BASE_DIR="../../../../../network_drive"
-RSBOLD_DIR_ET="../../../../data/cvr/rsBOLD_ET"
-RSBOLD_DIR_ET_FLIP="../../../../data/cvr/rsBOLD_ET_Flip"
-T1_DIR="../../../../data/cvr/T1"
+
+IN_BASE_DIR="../../../../data/cvr/"
 OUTPUT_BASE_DIR=$PWD
 
-./preprocess_fmri_anatomical.sh $RSBOLD_DIR_ET $T1_DIR $OUTPUT_BASE_DIR 19570-20220112-SF01029
-#./batch_preprocess_fmri_anat.sh /path/to/rsBOLD_ET /path/to/T1 /path/to/output all
+#./preprocess_fmri_anatomical.sh $IN_BASE_DIR $OUTPUT_BASE_DIR 19795-20220304-SF_01030 19797-20220304-SF_01033
+./preprocess_fmri_anatomical.sh $IN_BASE_DIR $OUTPUT_BASE_DIR all

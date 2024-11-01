2471eb7869674f8f5e4f56f7541d18180cd69ca8
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-11-01 01:54:54 -0600

    prep for analysis on cluster

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=00:10:00
-#SBATCH --mem=1GB
+#SBATCH --cpus-per-task=4
+#SBATCH --time=00:30:00
+#SBATCH --mem=48GB
 #SBATCH --job-name=MyFirstJobOnARC
-#SBATCH --partition=cpu2023
+##SBATCH --partition=cpu2023
 #SBATCH --mail-type=END
 #SBATCH --mail-user=muhammad.mahajna@ucalgary.ca
 #SBATCH --output=OutputFromMyFirstJob_%j.out
@@ -18,7 +18,6 @@ echo Hello World
 IN_BASE_DIR="../../../../data/cvr"
 OUTPUT_BASE_DIR=$IN_BASE_DIR
 
-./preprocess_fmri_anatomical.sh $IN_BASE_DIR $OUTPUT_BASE_DIR 19797-20220304-SF_01033
-
+./preprocess_raw_data.sh 
 
 echo Finished model training
--- src/preprocessing/preprocess_raw_data.sh
+++ src/preprocessing/preprocess_raw_data.sh
@@ -2,10 +2,10 @@
 IN_BASE_DIR="../../../../data/cvr"
 OUTPUT_BASE_DIR=$IN_BASE_DIR
 
-#./preprocess_fmri_anatomical.sh $IN_BASE_DIR $OUTPUT_BASE_DIR 19797-20220304-SF_01033
+./preprocess_fmri_anatomical.sh $IN_BASE_DIR $OUTPUT_BASE_DIR 19797-20220304-SF_01033
 #./preprocess_fmri_anatomical.sh $IN_BASE_DIR $OUTPUT_BASE_DIR all
 
-#./prepare_and_register_with_ants.sh $IN_BASE_DIR 19797-20220304-SF_01033
+./prepare_and_register_with_ants.sh $IN_BASE_DIR 19797-20220304-SF_01033
 #./prepare_and_register_with_ants.sh $IN_BASE_DIR all
 
 ./normalize_and_extract_roi.sh $IN_BASE_DIR 19797-20220304-SF_01033

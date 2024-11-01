8c3f0d3700685bca41754fff48c93d94f4bd51bf
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-11-01 10:30:41 -0600

    ANTS defaul dir export

--- utils/prep_cluster_env.sh
+++ utils/prep_cluster_env.sh
@@ -3,3 +3,4 @@ export FSLDIR=/home/muhammad.mahajna/miniconda3/envs/FSL
 source ${FSLDIR}/etc/fslconf/fsl.sh
 export PATH=${FSLDIR}/bin:$PATH
 export FSLOUTPUTTYPE=NIFTI_GZ
+export PATH="/home/muhammad.mahajna/software/ants-2.5.3/bin:$PATH"

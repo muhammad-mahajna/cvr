a7a5c081ee644c0b2731ce6e61fc00245edf442d
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-11-04 16:41:37 -0700

    Updated the script for running all subjects in parallel - create a job for each subject -- test on 10 subjects

--- src/preprocessing/preproc_all_subs__slurm_job.sh
+++ src/preprocessing/preproc_all_subs__slurm_job.sh
@@ -3,9 +3,9 @@
 #SBATCH --array=0-9 ## Test this with 10 subjects
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --time=02:00:00
-#SBATCH --mem=128GB
+#SBATCH --mem=80GB
 ##SBATCH --mail-type=END
 ##SBATCH --mail-user=muhammad.mahajna@ucalgary.ca
 #SBATCH --output=OutputFromCVRJob_%A_%a.out
@@ -29,7 +29,7 @@ SUBJECT_LIST=($(ls -d $IN_BASE_DIR/*/ | xargs -n 1 basename))
 echo "Going to process the following subjects: $SUBJECT_LIST"
 
 # Select the subject based on the job array index
-SUBJECT_ID=${SUBJECT_LIST[$SLURM_ARRAY_TASK_ID]}
+SUBJECT_ID=${SUBJECT_LIST[$SLURM_ARRAY_TASK_ID+1]}
 
 # Run the processing script for the selected subject
 ./preprocess_raw_data.sh $IN_BASE_DIR $OUTPUT_BASE_DIR $SUBJECT_ID
--- src/preprocessing/preproc_single_sub__slurm_job.sh
+++ src/preprocessing/preproc_single_sub__slurm_job.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --time=02:00:00
-#SBATCH --mem=128GB
+#SBATCH --mem=80GB
 #SBATCH --job-name=CVR
-##SBATCH --partition=cpu2023
+#SBATCH --partition=cpu2023
 #SBATCH --mail-type=END
 #SBATCH --mail-user=muhammad.mahajna@ucalgary.ca
 #SBATCH --output=OutputFromMyCVRJob_%j.out
@@ -16,6 +16,8 @@ echo Hi, lets start working...
 # Define base directories
 IN_BASE_DIR="../../../../data/cvr"
 OUTPUT_BASE_DIR=$IN_BASE_DIR
+
+echo Load Conda Env and get things ready
 
 # Load the conda environment
 eval "$(~/miniconda3/bin/conda shell.bash hook)"

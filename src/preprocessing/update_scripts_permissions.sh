fdc61231b2ad589d23ffa229180635600dc847b3
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-11-14 15:06:56 -0700

    Updated SLURM scripts

--- src/preprocessing/update_scripts_permissions.sh
+++ src/preprocessing/update_scripts_permissions.sh
@@ -9,3 +9,4 @@ chmod +x ./preprocess_ref_cvr_data.sh
 chmod +x ./preproc_single_sub__test.sh
 chmod +x ./process_cvr_maps.sh
 chmod +x ./register_cvr_to_anat.sh
+chmod +x ./preproc_single_sub__slurm_job.slurm

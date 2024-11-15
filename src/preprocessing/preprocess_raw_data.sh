23b9d6d67d5e648bd9faf8aa3f93b7a79709313b
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-11-15 13:39:01 -0700

    dir fixes

--- src/preprocessing/preprocess_raw_data.sh
+++ src/preprocessing/preprocess_raw_data.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <input_base_directory> <output_base_directory> <subject_id>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <input_base_directory> <output_base_directory> <subject_id> <script_dir>"
     exit 1
 fi
 
-# Get the input and output base directories and the subject ID from the script arguments
+# Get the input and output base directories, subject ID, and script directory
 IN_BASE_DIR="$1"
 OUTPUT_BASE_DIR="$2"
 SUBJECT_ID="$3"
-
-# Resolve the directory of the current script
-SCRIPT_DIR=$(realpath $(dirname "$0"))
+SCRIPT_DIR="$4"
 
 # Check if the subject directory exists
 if [ ! -d "$IN_BASE_DIR/$SUBJECT_ID" ]; then
@@ -22,11 +20,11 @@ fi
 
 echo "Processing subject: $SUBJECT_ID"
 
-# Run each preprocessing step for the specific subject, using the resolved script directory
-bash "$SCRIPT_DIR/preprocess_fmri_anatomical.sh" "$IN_BASE_DIR" "$OUTPUT_BASE_DIR" "$SUBJECT_ID"
-bash "$SCRIPT_DIR/prepare_and_register_with_ants.sh" "$IN_BASE_DIR" "$SUBJECT_ID"
+# Run each preprocessing step for the specific subject
+bash "$SCRIPT_DIR/preprocess_fmri_anatomical.sh" "$IN_BASE_DIR" "$OUTPUT_BASE_DIR" "$SUBJECT_ID" "$SCRIPT_DIR"
+bash "$SCRIPT_DIR/prepare_and_register_with_ants.sh" "$IN_BASE_DIR" "$SUBJECT_ID" "$SCRIPT_DIR"
 
 # Uncomment if CVR preprocessing is required
-# bash "$SCRIPT_DIR/preprocess_ref_cvr_data.sh" "$IN_BASE_DIR" "$OUTPUT_BASE_DIR" "$SUBJECT_ID"
+# bash "$SCRIPT_DIR/preprocess_ref_cvr_data.sh" "$IN_BASE_DIR" "$OUTPUT_BASE_DIR" "$SUBJECT_ID" "$SCRIPT_DIR"
 
 echo "Preprocessing complete for subject: $SUBJECT_ID"

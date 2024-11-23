3a7f82a3e7e90001c447ffdfb5d2901bb849b234
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-11-22 22:15:21 -0700

    Improved data preprocessing

--- src/model/utils.py
+++ src/model/utils.py
@@ -5,7 +5,7 @@ def load_tf(input):
     img = nib.load(input)
     return img.get_fdata()
 
-def process_file(filepath, is_target=False, remove_time_points=5):
+def process_file(filepath, is_target=False, remove_time_points=5, data_threshold=0, max_zeros=10):
     # remove_time_points: remove the first remove_time_points because they might be noisy
     data = load_tf(filepath)
     
@@ -16,5 +16,11 @@ def process_file(filepath, is_target=False, remove_time_points=5):
         # Inputs: Remove initial time points and reshape
         data = data[..., remove_time_points:]  # Slice the time dimension
         reshaped = data.reshape((-1, data.shape[-1]))  # Inputs: (num_voxels, remaining_time_points)
-    
+        # Zero values smaller than the threshold
+        if data_threshold > 0:
+            reshaped[reshaped < data_threshold] = 0
+
+    zero_counts = np.sum(reshaped==0, axis=1)  # Count zeros along the time dimension
+    reshaped[zero_counts > max_zeros, :] = 0
+
     return reshaped
